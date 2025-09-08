bcd0b04533531ab67a36ee1c81bef98c12de2244
MemoFlashW <L22170838@culiacan.tecnm.mx>
2025-09-07 19:28:22 -0700

    Ideas de memoU

---
+++ Puzzle8Prueba/Nodo.py
@@ -0,0 +1,19 @@
+from Tablero import tablero
+from copy import deepcopy
+
+class Nodo:
+    def _init_(self, father=None, nums=[],dir='l'):
+        self.father = father
+        if self. father is None:
+            self.Tablero = Tablero(nums)
+        else:
+            self.Tablero = deepcopy(self.father.Tablero)
+            self.Tablero.mover(dir=dir)
+            
+    def setChild(self):
+        self.childs = []
+        for _dir in self.Taclero.moves():
+            auxN = Nodo(father=self, dir=_dir)
+            self.childs(append(auxN))
+            
+            
---
+++ Puzzle8Prueba/Tablero.py
@@ -0,0 +1,40 @@
+sol =  [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 0]]
+
+class Tablero:
+    def __init__(self,nums=[[1,2,3],[4,5,6],[7,8,0]]):
+        self.nums = nums 
+        
+        def Solucion(self):
+            return self.nums == sol
+    
+    def vacio(self):
+        for ir, r in enumerate(self.nums):
+            for ic, c in enumerate(r):
+                if c == 0:
+                    return ir, ic
+                
+    def moves(self):
+        r,c = self.vacio()
+        return movimientos[r][c]          
+    
+    def moverse(self, dir):
+        r,c = self.vacio()
+        
+        if dir=='l':
+            aux = self.nums[r][c]
+            self.nums[r][c] = self.nums[r][c-1]
+            self.nums[r][c-1] = aux
+        elif dir=='r':
+            aux = self.nums[r][c]
+            self.nums[r][c] = self.nums[r][c+1]
+            self.nums[r][c+1] = aux
+        elif dir=='u':
+            aux = self.nums[r][c]
+            self.nums[r][c] = self.nums[r-1][c]
+            self.nums[r-1][c] = aux
+        elif dir=='d':
+            aux = self.nums[r][c]
+            self.nums[r][c] = self.nums[r+1][c]
+            self.nums[r+1][c] = aux
